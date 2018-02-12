2869b4ec1604db5492b2ac03d7c2c2fc36cc6da4
Vincent Redouté <vincent.redoute@live.fr>
2018-02-12 22:30:53 +0100

    creation de la classe automate

---
+++ automate.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using namespace std;
+
+class Automate {
+public:
+    Automate(){  }
+    virtual ~Automate() { }
+    void lecture();
+
+protected:
+    string chaine;
+};
+
--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include "lexer.h"
+#include "automate.h"
 
 
 int main(void) {
-   string chaine("(1+34)*123");
 
-   Lexer l(chaine);
+   Automate automate;
+   automate.lecture();
 
-   Symbole * s;
-   while(*(s=l.Consulter())!=FIN) {
-      s->Affiche();
-      cout<<endl;
-      l.Avancer();
-   }
    return 0;
 }
 
