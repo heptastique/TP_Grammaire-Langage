804d9d1d7a59d4cc594ce6b34e170e2615f79da8
Pache <etienne.raquin@insa-lyon.fr>
2018-02-14 08:35:32 +0100

    Implementation de Etat et Automate en cours

---
+++ automate.h
@@ -0,0 +1,29 @@
+# include <stack>
+# include "lexer.h"
+
+using namespace std;
+
+class Etat;
+
+class Automate
+{
+	public:
+
+		Automate();
+		virtual ~Automate();
+
+		// Lecture de la chaine de caracteres entree
+		void lecture();
+
+		// Empile un symbole et un etat
+		void decalage(Symbole * symbole, Etat * etat);
+
+		void reduction(int n, Symbole * symbole);
+
+	protected:
+
+		string chaine;
+
+		stack <Etat *> etats;
+		stack <Symbole *> symboles;
+};
