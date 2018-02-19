6c36e5fface092280f161e04d0cd97b0a7808326
Victor <vb4007@gmail.com>
2018-02-19 17:58:14 +0100

    Fix compilation error
    what the ((heck))

--- e3.cpp
+++ e3.cpp
@@ -5,12 +5,14 @@ E3::E3(string name) : Etat(name) {}
 E3::~E3() {}
 
 bool E3::transition(Automate &automate, Symbole *s) {
+    Symbole * s1;
+
     switch (*s) {
         case PLUS:
         case MULT:
         case CLOSEPAR:
         case FIN:
-            Symbole * s1 = automate.pop();
+            s1 = automate.pop();
             s1->setIdent(5);
             automate.reduction(1, s1);
             break;
